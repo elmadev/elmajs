0a951751768a5376f7bc88bd5246f760d43d30ed
Roger Andersen <hexjelly@hexjelly.com>
2016-10-02 13:41:01 +0200

    Add simple nullpadString function for testing

--- src/util.js
+++ src/util.js
@@ -3,6 +3,18 @@ function formatTime () {
 }
 
 /**
+ * Pads string with null bytes.
+ * @param {string} str String to pad
+ * @param {integer} pad Amount to pad
+ * @returns {string} string
+ */
+function nullpadString (str, pad) {
+  // check if ascii or padding shorter than string
+  if ((!/^[\x00-\x7F]*$/.test(str)) || (str.length > pad)) return undefined
+  return str + Array(pad - str.length + 1).join('\x00')
+}
+
+/**
  * Trims off null bytes and any additional garbage bytes from a buffer.
  * @param {Buffer} buffer Buffer to parse string from
  * @returns {string} string
@@ -20,5 +32,6 @@ function trimString (buffer) {
 
 module.exports = {
   formatTime,
+  nullpadString,
   trimString
 }
