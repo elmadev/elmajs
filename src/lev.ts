335d9041182195513fa5065081248a22b457da2e
Roger Andersen <hexjelly@hexjelly.com>
2018-11-03 14:21:57 +0100

    Add .toBuffer() method

--- src/lev.ts
+++ src/lev.ts
@@ -3,7 +3,7 @@ import { ElmaObject, Gravity, ObjectType } from './lev/ElmaObject'
 import { Picture } from './lev/Picture'
 import { Polygon } from './lev/Polygon'
 import { OBJECT_RADIUS, Position } from './shared'
-import { trimString } from './util'
+import { nullpadString, trimString } from './util'
 
 const EOD_MARKER = 0x0067103a // level data marker
 const EOF_MARKER = 0x00845d52 // level file marker
@@ -25,6 +25,10 @@ export interface ITop10 {
 }
 
 export class Level {
+  /**
+   * Loads a level file.
+   * @param source Can either be a file path or a buffer
+   */
   public static async load(source: string | Buffer): Promise<Level> {
     if (typeof source === 'string') {
       const file = await readFile(source)
@@ -35,6 +39,9 @@ export class Level {
     throw new Error('This should be unreachable but ok')
   }
 
+  /**
+   * Encrypts or decrypts top10 lists
+   */
   public static cryptTop10(buffer: Buffer): Buffer {
     const output = Buffer.from(buffer) // copy buffer to not modify reference
     let ebp8 = 0x15
@@ -289,5 +296,174 @@ export class Level {
       multi: [],
       single: [],
     }
+  }
+
+  /**
+   * Returns a buffer representation of the level.
+   */
+  public async toBuffer(): Promise<Buffer> {
+    this.integrity = this._calculateIntegrity()
+    let bufferSize = 850 // all known level attributes' size
+    for (const polygon of this.polygons) {
+      bufferSize += 8 + 16 * polygon.vertices.length
+    }
+    bufferSize += 28 * this.objects.length + 54 * this.pictures.length
+    const buffer = Buffer.alloc(bufferSize)
+
+    if (this.version !== Version.Elma) {
+      throw new Error('Only Elma levels are supported')
+    }
+    buffer.write('POT14', 0, 5, 'ascii')
+    buffer.writeUInt16LE(this.link & 0xffff, 5)
+    buffer.writeUInt32LE(this.link, 7)
+    for (let i = 0; i < this.integrity.length; i++) {
+      buffer.writeDoubleLE(this.integrity[i], 11 + i * 8)
+    }
+    const name = nullpadString(this.name, 51)
+    buffer.write(name, 43, 51, 'ascii')
+    const lgr = nullpadString(this.lgr, 16)
+    buffer.write(lgr, 94, 16, 'ascii')
+    const ground = nullpadString(this.ground, 10)
+    buffer.write(ground, 110, 10, 'ascii')
+    const sky = nullpadString(this.sky, 10)
+    buffer.write(sky, 120, 10, 'ascii')
+
+    buffer.writeDoubleLE(this.polygons.length + 0.4643643, 130)
+    let offset = 138 // unknown territory, time to keep track of offset!
+    for (const polygon of this.polygons) {
+      buffer.writeInt32LE(polygon.grass ? 1 : 0, offset)
+      offset += 4
+      buffer.writeInt32LE(polygon.vertices.length, offset)
+      offset += 4
+      for (const vertex of polygon.vertices) {
+        buffer.writeDoubleLE(vertex.x, offset)
+        offset += 8
+        buffer.writeDoubleLE(-vertex.y, offset)
+        offset += 8
+      }
+    }
+
+    buffer.writeDoubleLE(this.objects.length + 0.4643643, offset)
+    offset += 8
+    for (const obj of this.objects) {
+      buffer.writeDoubleLE(obj.position.x, offset)
+      offset += 8
+      buffer.writeDoubleLE(-obj.position.y, offset)
+      offset += 8
+      buffer.writeInt32LE(obj.type, offset)
+      offset += 4
+      buffer.writeInt32LE(obj.gravity, offset)
+      offset += 4
+      buffer.writeInt32LE(obj.animation - 1, offset)
+      offset += 4
+    }
+
+    buffer.writeDoubleLE(this.pictures.length + 0.2345672, offset)
+    offset += 8
+    for (const picture of this.pictures) {
+      const picName = nullpadString(picture.name, 10)
+      buffer.write(picName, offset, 10, 'ascii')
+      offset += 10
+      const texture = nullpadString(picture.texture, 10)
+      buffer.write(texture, offset, 10, 'ascii')
+      offset += 10
+      const mask = nullpadString(picture.mask, 10)
+      buffer.write(mask, offset, 10, 'ascii')
+      offset += 10
+      buffer.writeDoubleLE(picture.position.x, offset)
+      offset += 8
+      buffer.writeDoubleLE(-picture.position.y, offset)
+      offset += 8
+      buffer.writeInt32LE(picture.distance, offset)
+      offset += 4
+      buffer.writeInt32LE(picture.clip, offset)
+      offset += 4
+    }
+
+    buffer.writeInt32LE(EOD_MARKER, offset)
+    offset += 4
+    this._top10ToBuffer().copy(buffer, offset)
+    offset += 688
+    buffer.writeInt32LE(EOF_MARKER, offset)
+
+    return buffer
+  }
+
+  /**
+   * Calculates the integrity sums of the level.
+   * NOTE: Does not currently detect topology errors.
+   */
+  private _calculateIntegrity(): number[] {
+    const polSum = this.polygons.reduce((polyAccumulator, polyCurrent) => {
+      return (
+        polyAccumulator +
+        polyCurrent.vertices.reduce((vertAccumulator, vertCurrent) => {
+          return vertAccumulator + vertCurrent.x + vertCurrent.y
+        }, 0)
+      )
+    }, 0)
+
+    const objSum = this.objects.reduce((objAccumulator, objCurrent) => {
+      let objVal = 0
+      if (objCurrent.type === ObjectType.Exit) objVal = 1
+      else if (objCurrent.type === ObjectType.Apple) objVal = 2
+      else if (objCurrent.type === ObjectType.Killer) objVal = 3
+      else if (objCurrent.type === ObjectType.Start) objVal = 4
+      return (
+        objAccumulator + objCurrent.position.x + objCurrent.position.y + objVal
+      )
+    }, 0)
+
+    const picSum = this.pictures.reduce((picAccumulator, picCurrent) => {
+      return picAccumulator + picCurrent.position.x + picCurrent.position.y
+    }, 0)
+
+    const sum = (polSum + objSum + picSum) * 3247.764325643
+
+    return [
+      sum,
+      this._getRandomInt(0, 5871) + 11877 - sum,
+      this._getRandomInt(0, 5871) + 11877 - sum,
+      this._getRandomInt(0, 6102) + 12112 - sum,
+    ]
+  }
+
+  /**
+   * Returns a random number in [min..max] range
+   */
+  private _getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  /**
+   * Maps over top10 lists and generates the encrypted buffer.
+   */
+  private _top10ToBuffer() {
+    const buffers: Buffer[] = Object.values(this.top10)
+      .reverse() // ordered keys "multi" and "single", so reverse them
+      .map(list => {
+        // sort all the times first
+        list.sort((a: ITimeEntry, b: ITimeEntry) => {
+          if (a.time > b.time) return 1
+          if (a.time < b.time) return -1
+          return 0
+        })
+        const buffer = Buffer.alloc(344)
+        buffer.writeUInt32LE(list.length >= 10 ? 10 : list.length, 0)
+
+        for (let i = 0; i < list.length; i++) {
+          if (i < 10) {
+            buffer.writeUInt32LE(list[i].time, 4 + 4 * i)
+            buffer.write(nullpadString(list[i].name1, 15), 44 + 15 * i)
+            buffer.write(nullpadString(list[i].name2, 15), 194 + 15 * i)
+          }
+        }
+
+        return buffer
+      })
+
+    return Level.cryptTop10(Buffer.concat(buffers, 688))
   }
 }
