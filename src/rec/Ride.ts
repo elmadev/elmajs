9db2851d895bd5810e6d4fc2c63c7414dd58dcca
Roger Andersen <hexjelly@hexjelly.com>
2018-11-11 17:51:37 +0100

    Fix remaining rec parsing

--- src/rec/Ride.ts
+++ src/rec/Ride.ts
@@ -7,12 +7,39 @@ export interface IRideHeader {
   isFlagTag: boolean
 }
 
+export enum Direction {
+  Left = 0,
+  Right = 1,
+}
+
 export interface IFrame {
-  position: Position
+  bike: Position
+  leftWheel: Position
+  rightWheel: Position
+  head: Position
+  bikeRotation: number
+  leftWheelRotation: number
+  rightWheelRotation: number
+  throttle: boolean
+  direction: Direction
+  backWheelSpeed: number
+  collisionStrength: number
 }
 
 export interface IEvent {
-  type: boolean
+  type: EventType
+  time: number
+  touchInfo: number
+  groundInfo: number
+}
+
+export enum EventType {
+  Touch = 0,
+  Ground = 1,
+  Apple = 4,
+  Turn = 5,
+  VoltRight = 6,
+  VoltLeft = 7,
 }
 
 export default class Ride {
