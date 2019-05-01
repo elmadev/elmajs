afc18c854602512c144452c34d278e30850cc10c
Roger Andersen <hexjelly@hexjelly.com>
2019-05-01 13:14:27 +0200

    feat(lgr): write lgr

--- __tests__/lgr.int.test.ts
+++ __tests__/lgr.int.test.ts
@@ -1,15 +1,17 @@
 import { LGR } from '../src';
 
 describe('LGR', () => {
-  test('load and save LGR creates same file #1', async () => {
+  test('load and toBuffer matches #1', async () => {
     const lgr = await LGR.load('__tests__/assets/lgr/Default.lgr');
+    lgr.path = '';
     const lgrBuffer = await lgr.toBuffer();
     const loadedBuffer = await LGR.load(lgrBuffer);
     expect(lgr).toEqual(loadedBuffer);
   });
 
-  test('load and save LGR creates same file #2', async () => {
+  test('load and toBuffer matches #2', async () => {
     const lgr = await LGR.load('__tests__/assets/lgr/Across.lgr');
+    lgr.path = '';
     const lgrBuffer = await lgr.toBuffer();
     const loadedBuffer = await LGR.load(lgrBuffer);
     expect(lgr).toEqual(loadedBuffer);
