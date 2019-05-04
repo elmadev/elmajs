f5a981d8903159f37c8e21e322a5ea9d2409667f
Roger Andersen <hexjelly@hexjelly.com>
2019-05-04 12:36:24 +0200

    test(state): add test case

--- __tests__/state.int.test.ts
+++ __tests__/state.int.test.ts
@@ -1,4 +1,4 @@
-import { State } from '../src';
+import { PlayMode, SoundOptimization, State, VideoDetail } from '../src';
 
 describe('State', () => {
   test('load and toBuffer matches #1', async () => {
@@ -15,5 +15,19 @@ describe('State', () => {
     const stateBuffer = await state.toBuffer();
     const loadedBuffer = await State.load(stateBuffer);
     expect(state).toEqual(loadedBuffer);
+  });
+
+  test('state matches expected output', async () => {
+    const state = await State.load('__tests__/assets/state/state.dat');
+
+    const expectedTimes = {};
+    expect(state.times[0]).toEqual(expectedTimes);
+    expect(state.playMode).toBe(PlayMode.Single);
+    expect(state.soundEnabled).toBe(false);
+    expect(state.soundOptimization).toBe(SoundOptimization.Compatibility);
+    expect(state.animatedMenus).toBe(true);
+    expect(state.videoDetail).toBe(VideoDetail.High);
+    expect(state.animatedObjects).toBe(true);
+    expect(state.swapBikes).toBe(false);
   });
 });
