24aceffaa7029d201bc5c47991e186298af36c4a
Roger Andersen <hexjelly@hexjelly.com>
2020-05-23 03:19:01 +0200

    refactor(LGR): remove async, use buffer lib

--- __tests__/lgr.int.test.ts
+++ __tests__/lgr.int.test.ts
@@ -1,19 +1,13 @@
+import { readFile } from 'fs-extra';
+
 import { LGR } from '../src';
 
 describe('LGR', () => {
-  test('load and toBuffer matches #1', async () => {
-    const lgr = await LGR.load('__tests__/assets/lgr/Default.lgr');
-    lgr.path = '';
-    const lgrBuffer = await lgr.toBuffer();
-    const loadedBuffer = await LGR.load(lgrBuffer);
-    expect(lgr).toEqual(loadedBuffer);
-  });
-
-  test('load and toBuffer matches #2', async () => {
-    const lgr = await LGR.load('__tests__/assets/lgr/Across.lgr');
-    lgr.path = '';
-    const lgrBuffer = await lgr.toBuffer();
-    const loadedBuffer = await LGR.load(lgrBuffer);
+  test.each(['Default.lgr', 'Across.lgr'])('parsing and toBuffer matches: %s', async (fileName: string) => {
+    const file = await readFile(`__tests__/assets/lgr/${fileName}`);
+    const lgr = LGR.from(file);
+    const lgrBuffer = lgr.toBuffer();
+    const loadedBuffer = LGR.from(lgrBuffer);
     expect(lgr).toEqual(loadedBuffer);
   });
 });
--- src/lgr/index.ts
+++ src/lgr/index.ts
@@ -1,7 +1,3 @@
 export { default as LGR } from './LGR';
 export { default as PictureData } from './PictureData';
-export {
-  default as PictureDeclaration,
-  PictureType,
-  Transparency,
-} from './PictureDeclaration';
+export { default as PictureDeclaration, PictureType, Transparency } from './PictureDeclaration';
