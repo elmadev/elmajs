b92d085c02859560b94c84d8b96f308e881d3420
GauChoob <starlight_tonight@hotmail.com>
2025-01-29 18:15:44 -0500

    feat(lgr): check an lgr's palette

--- __tests__/lgr.int.test.ts
+++ __tests__/lgr.int.test.ts
@@ -2,12 +2,21 @@ import { readFile } from 'fs-extra';
 
 import { LGR } from '../src';
 
-describe('LGR', () => {
-  test.each(['Default.lgr', 'Across.lgr'])('parsing and toBuffer matches: %s', async (fileName: string) => {
+describe.each([
+  ['Default.lgr', true],
+  ['Across.lgr', true],
+  ['botanic.lgr', false],
+])('LGR', (fileName, isDefaultPalette) => {
+  test(`parsing and toBuffer matches: ${fileName}`, async () => {
     const file = await readFile(`__tests__/assets/lgr/${fileName}`);
     const lgr = LGR.from(file);
     const lgrBuffer = lgr.toBuffer();
     const loadedBuffer = LGR.from(lgrBuffer);
     expect(lgr).toEqual(loadedBuffer);
+  });
+  test(`${fileName}.lgr's palette is the default palette: ${isDefaultPalette}`, async () => {
+    const file = await readFile(`__tests__/assets/lgr/${fileName}`);
+    const lgr = LGR.from(file);
+    expect(lgr.paletteIsDefault()).toEqual(isDefaultPalette);
   });
 });
--- src/lgr/PictureData.ts
+++ src/lgr/PictureData.ts
@@ -1,9 +1,13 @@
+import PCX from 'pcx-js';
+
 export default class PictureData {
   public name: string;
   public data: Buffer;
+  public image: PCX;
 
   constructor(name: string, buffer: Buffer) {
     this.name = name;
     this.data = buffer;
+    this.image = new PCX(this.data);
   }
 }
